c244d10e6a9d9e0c3025d52bd1ce38958bb09bbd
ling11141120 <ling11141120@126.com>
2025-06-12 16:10:16 +0800

    增加了报表统计和云日记的地图功能

---
+++ src/main/java/com/ling/note/vo/NoteVo.java
@@ -0,0 +1,8 @@
+package com.ling.note.vo;
+
+/**
+ * &#064;Author:  LingWeiBo
+ * &#064;Date:  2025/6/12 09:04
+ */
+public class NoteVo {
+}
---
+++ src/main/java/com/ling/note/web/ReportServlet.java
@@ -0,0 +1,8 @@
+package com.ling.note.web;
+
+/**
+ * &#064;Author:  LingWeiBo
+ * &#064;Date:  2025/6/12 10:39
+ */
+public class ReportServlet {
+}
